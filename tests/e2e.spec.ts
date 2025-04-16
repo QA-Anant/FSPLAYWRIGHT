ebbdbf8e26283b5033dca11ad533b449ae1900de
anant.jain <anant.jain@SDI-HLANANTJ.local>
2025-04-16 19:32:40 +0530

    day 16 changes

--- tests/e2e.spec.ts
+++ tests/e2e.spec.ts
@@ -46,10 +46,60 @@ test.skip("tc2", async ({ page }) => {
 
 // Day 15 : Waiting strategies and Timeouts
 
-test ('tc_day15_1', async ({ page }) => {
+test.skip ('tc_day15_1', async ({ page }) => {
   test.setTimeout(40000);
   await page.goto('https://demo.evershop.io/account/login', { timeout: 30000 }); // Set timeout for page navigation at the test level
 
   let l1 = page.locator("input[name='email']");
   await l1.fill("anant"); // auto-wait
 });
+
+test.skip("tc_day16_1", async({page})=>{
+
+  await page.goto('https://demo.evershop.io/account/login', { timeout: 30000 }); // Set timeout for page navigation at the test level
+  await page.locator("input[name='email']").fill("jain.anant4567@gmail.com"); // auto-wait
+  await page.locator("input[name='password']").fill("test@123"); // auto-wait
+  await page.locator("button[type='submit']").click(); // auto-wait
+
+  let l1 = page.locator("//a/span[text()='Shop kids']")
+  await l1.waitFor({state: "visible", timeout: 50000}); // auto-wait
+
+  await page.goto('https://demo.evershop.io/checkout', { timeout: 30000 }); // Set timeout for page navigation at the test level
+
+
+  await page.locator("//select[@id='address[country]']").selectOption({label: "India"}); // auto-wait
+  //await page.pause();
+})
+
+
+test("tc_day16_2", async({page})=>{
+
+  await page.goto('https://demo.evershop.io/account/login', { timeout: 30000 }); // Set timeout for page navigation at the test level
+  await page.locator("input[name='email']").fill("jain.anant4567@gmail.com"); // auto-wait
+  await page.locator("input[name='password']").fill("test@123"); // auto-wait
+  await page.locator("button[type='submit']").click(); // auto-wait
+
+  let l1 = page.locator("//a/span[text()='Shop kids']")
+  await l1.waitFor({state: "visible", timeout: 50000}); // auto-wait
+
+  await page.goto('https://demo.evershop.io/cart', { timeout: 30000 }); // Set timeout for page navigation at the test level
+
+  let columncount = await page.locator("table thead tr td").count();
+  console.log("column count is :", columncount);
+
+  let columns = await page.locator("table thead tr td").all();
+
+  let columnNames : string[] = [];
+
+  for(const column of columns)
+  {
+    let columnName = await column.locator("span").innerText();
+    columnNames.push(columnName);
+  }
+
+  columnNames.forEach((columnName) => {
+    console.log("column name is :", columnName);
+  })
+  
+  await page.pause();
+})
