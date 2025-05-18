c6d2c79e1cc94d078c4729f26ce95205a26b88e8
anant.jain <anant.jain@SDI-HLANANTJ.local>
2025-05-18 23:57:49 +0530

    changes

--- tests/specs/e2e.spec.ts
+++ tests/specs/e2e.spec.ts
@@ -1,14 +1,31 @@
 import test from "@playwright/test";
 import LoginPom from "../pom/loginPom";
 import HomePom from "../pom/homePom";
+import { readJsonDataForTestCase } from "../utils/fileUtil";
+import * as Path from 'path';
+import { LoginDataLayer } from "../dataControllerLayer/loginDataLayer";
+import LoginInputDao from "../dao/inputDao/loginInputDao";
 
 test("Login Test", async ({ page }) => {
 
     let loginPom = new LoginPom(page);
 
     await loginPom.navigateTo();
-    let homePom : HomePom = await loginPom.submitCredentials("jain.anant4567@gmail.com", "test@123");
-    await homePom.productLinkClick();
+
+    const filePath = Path.join(process.cwd() + "/tests/testdata/testData.json");
+
+    const testdata = readJsonDataForTestCase(filePath, "TC1");
+
+    const loginData : LoginDataLayer =  testdata["login"];
+
+    const loginInputDaoInstance: LoginInputDao = new LoginInputDao(loginData);
+
+    // Json --> JS Object --> LoginDataLayer for compliance check --> DAO layer - InputDAO 
+
+    await loginPom.submitCredentials(loginInputDaoInstance);
+
+    // let homePom : HomePom = await loginPom.submitCredentials(loginInputDaoInstance);
+    // await homePom.productLinkClick();
 
   
   
